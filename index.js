4bcde1b230ca211e56a3df22b2d18fa461480384
el-papi-chiludo <vazqueznoriegagilberto@gmail.com>
2019-09-14 16:53:39 -0500

    update port 2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 const app = express()
 
@@ -8,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('hola mundo')
 })
  
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.listen(server_port,() => {
+  console.log( `server listen in port ${server_port}` )
 });
